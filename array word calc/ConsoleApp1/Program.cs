de774a41411f6bd4b0bca0bb67a19f17e2bd06a3
Kacprr <117747063+Kacprr@users.noreply.github.com>
2022-12-12 15:20:00 +0100

    Add files via upload

---
+++ array word calc/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+StreamReader sr = new StreamReader("C:\\Users\\Student\\Desktop\\data.txt");
+// or sr = new StreamReader(@"C:\Users\Student\Desktop\data.txt");\
+int wordcount = 0;
+int[] results = new int[20];
+while (!sr.EndOfStream)
+{
+    string line = sr.ReadLine();
+    string[] words = line.Split(" ");
+    for (int i = 0; i < words.Length; i++)
+    {
+        results[words[i].Length]++;
+        wordcount++;
+    }
+}
+for (int i = 0; i < 20; i++)
+{
+    Console.WriteLine(results[i]);
+}
+Console.WriteLine(wordcount);
+sr.Close();
